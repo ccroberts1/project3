5826ca188dadcfb858fb03ad38475a33802b6de9
Tom Highum <tomhighum@rocketmail.com>
2022-01-28 12:00:09 -0700

    Created the modals for Sign in and Sign up front end

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import SignInModal from './SignInModal';
+import SignUpModal from './SignUpModal';
 
 function NavBar() {
-  return <div></div>;
+    return (<>
+        <SignInModal></SignInModal>
+        <SignUpModal></SignUpModal>
+        
+        </>);
+    
 }
 
 export default NavBar;
