738c35a4a3c1806b3d35944d7155f3990ec5bafc
Olivia Hall <ohall1223@gmail.com>
2022-01-31 10:50:11 -0700

    seeds

---
+++ server/seeds/Data.js
@@ -0,0 +1,65 @@
+// import { Product } from '../models/Product'
+
+
+
+// const petPhotos = [
+//     {
+//       name: "A",
+//       description: "good boy",
+//       image: "https://images.dog.ceo/breeds/puggle/IMG_075427.jpg",
+//       price: 3,
+//       quantity: 3,
+//       categoty: "dog"
+//     },
+//     {
+//       name: "B",
+//       description: "good",
+//       image: "https://images.dog.ceo/breeds/pembroke/n02113023_14262.jpg",
+//       price: 4,
+//       quantity: 6,
+//       category: "dog"
+//     },
+//     {
+//       name: "C",
+//       description: 'sweet',
+//       image: "https://images.dog.ceo/breeds/dhole/n02115913_1677.jpg",
+//       price: 5,
+//       quantity: 1,
+//       category: "dog"
+//     },
+//     {
+//       name: "D",
+//       description: 'sweet',
+//       image: "https://cdn2.thecatapi.com/images/cuo.gif ",
+//       price: 5,
+//       quantity: 1,
+//       category: "cat" 
+//     },
+//     {
+//       name: "C",
+//       description: 'sweet',
+//       image: "https://thecatapi.com/api/images/get?format=src&type=gif",
+//       price: 5,
+//       quantity: 1,
+//       category: "cat"
+//     },
+//         {
+//       name: "C",
+//       description: 'sweet',
+//       image: "https://cdn2.thecatapi.com/images/4cl.gif"
+//       price: 5,
+//       quantity: 1,
+//       category: "cat"
+//     }
+    
+    
+    
+    
+//   ]
+
+//   const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)]
+
+
+// const genRandomProduct = (petPhotos) => arr[Math.floor(Math.random() * petPhotos.length)]
+
+// module.expoert = {getRandomProduct, getRandomArrItem}
