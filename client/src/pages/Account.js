087b50d22a9fd44110f468cb13840590cdf3f7ef
Charlie Highum <chighum41@gmail.com>
2022-02-01 16:29:35 -0700

    trying to render user info to account page

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { Grid, Dropdown, Input } from "semantic-ui-react";
+import { useQuery } from "@apollo/client";
+import { QUERY_USER } from "../utils/queries";
 
 function Account() {
   return <div>Account Information</div>;
