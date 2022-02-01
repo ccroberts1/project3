7c2c249e7e60750324f83d7637e8d032440fa759
Olivia Hall <ohall1223@gmail.com>
2022-01-31 18:31:49 -0700

    Improvements

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -18,13 +18,13 @@ connection.once('open', async () => {
 
     console.log("connected")
     
-    const url = ['https://api.thedogapi.com/v1/images/search?limit=20&page=10&order=Desc&mime_types=jpg,png']
+    const dogUrl = ['https://api.thedogapi.com/v1/images/search?limit=20&page=10&order=Desc&mime_types=jpg,png']
     
     const productData = []
 
-    url.map(async url => {
+    dogUrl.map(async dogUrl => {
         try {
-            const response = await fetch(url);
+            const response = await fetch(dogUrl);
             const json = await response.json();
             
             resultData = [...json];
@@ -52,6 +52,54 @@ connection.once('open', async () => {
                     const image = resultData[i]['url'];
                     const price = `${Math.floor(Math.random() * 100) + 1}`;
                     const quantity = `${Math.floor(Math.random() * 10) + 1}`;
+                    const category = '61f85380462540330ae499f5'
+                    
+                    productData.push({
+                        name,
+                        description,
+                        image,
+                        price,
+                        quantity,
+                        category,
+                    })
+            }
+            
+         
+        } catch (error) {
+            console.log(error);
+        }
+        console.log(productData)
+        const product = await Product.create(productData)
+        })
+
+        const catUrl = ['https://api.thecatapi.com/v1/images/search?limit=20&page=10&order=Desc&mime_types=jpg,png']
+         catUrl.map(async catUrl => {
+        try {
+            const response = await fetch(catUrl);
+            const json = await response.json();
+            
+            resultData = [...json];
+
+            // console.log(resultData)
+            // console.log(resultData[1]['breeds'])
+            
+            
+            for (let i = 0; i < 20; i++) {
+                    // console.log(resultData[i]['breeds'][0])
+                    // console.log(resultData[i])
+                    // console.log(resultData[0]['breeds'])
+                    let name;
+                    let description;
+                    if(resultData[i] === undefined || resultData[i]['breeds'][0] === undefined) {
+                        name = 'Mittens'
+                        description = 'good kitty'
+                    } else {
+                        name = resultData[i]['breeds'][0]['name'];
+                        description = resultData[i]['breeds'][0]['temperment']
+                    }
+                    const image = resultData[i]['url'];
+                    const price = `${(Math.floor(Math.random() * 100)) + 1}`;
+                    const quantity = `${(Math.floor(Math.random() * 10)) + 1}`;
                     const category = '61f85380462540330ae499f5'
                     
                     productData.push({
