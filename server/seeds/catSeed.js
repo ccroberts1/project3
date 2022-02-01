5547352436039212468490964a6c3cac829340ca
Olivia Hall <ohall1223@gmail.com>
2022-01-31 18:06:53 -0700

    cat seeds built

--- server/seeds/catSeed.js
+++ server/seeds/catSeed.js
@@ -1,7 +1,6 @@
-const userData = require("./userData.json");
 const mongoose = require('mongoose');
 const connection = require('../config/connection')
-const { Product, User } = require('../models')
+const { Product } = require('../models')
 // const db = require('../config/connection').mongoURI;
 const fetch = require('node-fetch')
 
@@ -10,18 +9,13 @@ let resultData;
 
 
 connection.once('open', async () => {
-    await User.deleteMany({});
-    const users = await User.insertMany(userData)
-    console.log("Users seeded");
-
     await Product.deleteMany()
 
     console.log("connected")
     
-    const url = ['https://api.thedogapi.com/v1/images/search?limit=20&page=10&order=Desc&mime_types=jpg,png']
+    const url = ['https://api.thecatapi.com/v1/images/search?limit=20&page=10&order=Desc&mime_types=jpg,png']
     
     const productData = []
-
     url.map(async url => {
         try {
             const response = await fetch(url);
@@ -30,7 +24,7 @@ connection.once('open', async () => {
             resultData = [...json];
 
             // console.log(resultData)
-            // console.log(resultData[0]['breeds'])
+            // console.log(resultData[1]['breeds'])
             
             
             for (let i = 0; i < 20; i++) {
@@ -40,18 +34,15 @@ connection.once('open', async () => {
                     let name;
                     let description;
                     if(resultData[i] === undefined || resultData[i]['breeds'][0] === undefined) {
-                        name = 'Spot'
+                        name = 'Mittens'
+                        description = 'good kitty'
                     } else {
                         name = resultData[i]['breeds'][0]['name'];
-                    }
-                     if(resultData[i] === undefined || resultData[i]['breeds'][0] === undefined || resultData[i]['breeds'][0]['temperment'] === undefined) {
-                        description = 'Good dog'
-                    } else {
                         description = resultData[i]['breeds'][0]['temperment']
                     }
                     const image = resultData[i]['url'];
-                    const price = `${Math.floor(Math.random() * 100) + 1}`;
-                    const quantity = `${Math.floor(Math.random() * 10) + 1}`;
+                    const price = `${(Math.floor(Math.random()) * 10) + 1}`;
+                    const quantity = `${(Math.floor(Math.random()) * 10) + 1}`;
                     const category = '61f85380462540330ae499f5'
                     
                     productData.push({
