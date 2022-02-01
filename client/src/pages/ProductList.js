66b6c20ab82400e6a2ae3ded12fccfe4f40bdc97
agate <nolimarie@protonmail.com>
2022-01-31 18:24:49 -0700

    added button for add to cart

--- client/src/pages/ProductList.js
+++ client/src/pages/ProductList.js
@@ -1,58 +1,73 @@
-import React from "react";
-import { useQuery } from "@apollo/client";
-import { Grid, Image, Card, Icon, Dropdown } from "semantic-ui-react";
-import { QUERY_ALL_PRODUCTS } from "../utils/queries";
+import React from 'react';
+import { Grid, Image, Card, Icon, Dropdown, Button, GridColumn } from 'semantic-ui-react'
 
 function ProductList() {
-  const { loading, data } = useQuery(QUERY_ALL_PRODUCTS);
-  console.log(data);
-  const products = data?.products || [];
-
   return (
-    <Grid centered center aligned columns={4} divided="vertically">
+    <Grid centered center aligned columns={4} divided='vertically'>
       <Grid.Column>Product List</Grid.Column>
       <Grid.Column>
         <Dropdown
-          text="Filter"
-          icon="filter"
-          floating
-          labeled
-          button
-          className="icon"
-        >
-          <Dropdown.Menu>
-            <Dropdown.Header icon="tags" content="Filter by tag" />
-            <Dropdown.Item>Cats</Dropdown.Item>
-            <Dropdown.Item>Dogs</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </Grid.Column>
-
+    text='Filter'
+    icon='filter'
+    floating
+    labeled
+    button
+    className='icon'
+  >
+    <Dropdown.Menu>
+      <Dropdown.Header icon='tags' content='Filter by tag' />
+      <Dropdown.Item>Cats</Dropdown.Item>
+      <Dropdown.Item>Dogs</Dropdown.Item>
+    </Dropdown.Menu>
+  </Dropdown></Grid.Column>
+   
       {/* Create Cards from Seeds with Image, Name, Description, Price, Quantity and Category */}
 
-      <Grid.Row columns={4}>
-        {products.map((product) => (
-          <Grid.Column>
+    <Grid.Row columns={4}>
+      <Grid.Column >
             <Card>
-              <Image src={product.image} wrapped ui={false} />
-              <Card.Content>
-                <Card.Header>{product.name}</Card.Header>
-                <Card.Description>{product.description}</Card.Description>
+    <Image src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*' wrapped ui={false} />
+    <Card.Content>
+      <Card.Header>Name</Card.Header>
+      <Card.Description>
+        Description
+              </Card.Description>
               </Card.Content>
-              <Card.Content>
-                <Card.Meta>Quantity Left: {product.description}</Card.Meta>
-              </Card.Content>
-              <Card.Content extra>
-                <a>
-                  <Icon name="dollar sign" />
-                  {product.price}
-                </a>
-              </Card.Content>
-            </Card>
-          </Grid.Column>
-        ))}
-      </Grid.Row>
-    </Grid>
+    <Card.Content>
+                    <Card.Meta>Quantity Left: </Card.Meta>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='dollar sign' />
+        1000.00
+      </a>
+        <Button>Add to Cart</Button>
+    </Card.Content>
+  </Card>
+      </Grid.Column>
+      <Grid.Column>
+        <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+        </Grid.Column>
+              <Grid.Column>
+        <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+        </Grid.Column>
+            <Grid.Column>
+        <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>
   );
 }
 
