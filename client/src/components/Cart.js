7dd66cb558a88456e739c5737ce1e319eb478990
Charlie Highum <chighum41@gmail.com>
2022-02-01 11:50:50 -0700

    found another issue with importing CartItem into Cart.js

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -4,7 +4,7 @@ import { loadStripe } from "@stripe/stripe-js";
 import { useLazyQuery } from "@apollo/client";
 import { QUERY_CHECKOUT } from "../utils/queries";
 import { idbPromise } from "../utils/helpers";
-import CartItem from "../CartItem";
+import CartItem from "./CartItem";
 import Auth from "../utils/auth";
 import { useStoreContext } from "../utils/StoreContext";
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../utils/actions";
