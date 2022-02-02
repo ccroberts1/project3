7df66297c92b6a462cb9a4d341a088685772a7b6
agate <nolimarie@protonmail.com>
2022-02-01 17:35:24 -0700

    styledd footer and home page

--- client/src/pages/Footer.js
+++ client/src/pages/Footer.js
@@ -1,24 +1,20 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 import { Container } from "semantic-ui-react";
+
+
 
 const Footer = () => {
   return (
-    <Container>
-      <h1
-        style={{
-          color: "white",
-          background: "black",
-          textAlign: "center",
-          marginTop: "25px",
-        }}
-      >
-        Links
-      </h1>
-      <br></br>
-      <a href="https://github.com/ccroberts1/project3.git">Github Repo</a>
-      <br></br>
-      <a href="https://raining-cats-and-dogs.herokuapp.com/">Heroku Deployed</a>
+    <Container fluid 
+     style={{
+      width: "100%",
+      color: "white",
+      background: "#1b1c1d",
+      textAlign: "center",  
+    }}>
+      <p>Links</p>
+      <a href="https://github.com/ccroberts1/project3.git">Github Repo</a><br/>
+      <a href="https://raining-cats-and-dogs.herokuapp.com/">Heroku Deployed App</a>
     </Container>
   );
 };
