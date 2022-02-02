e756f4d8ea184e310f500a96c461539ba86517d9
Charlie Highum <chighum41@gmail.com>
2022-02-01 20:37:31 -0700

    update user info form is working on account page

--- client/src/pages/Footer.js
+++ client/src/pages/Footer.js
@@ -1,21 +1,23 @@
 import React from "react";
 import { Container } from "semantic-ui-react";
 
-
-
 const Footer = () => {
   return (
-    <Container fluid 
-     style={{
-      width: "100%",
-      color: "white",
-      background: "#1b1c1d",
-      textAlign: "center",  
-    }}>
+    <div
+      style={{
+        width: "100%",
+        color: "white",
+        background: "#1b1c1d",
+        textAlign: "center",
+      }}
+    >
       <p>Links</p>
-      <a href="https://github.com/ccroberts1/project3.git">Github Repo</a><br/>
-      <a href="https://raining-cats-and-dogs.herokuapp.com/">Heroku Deployed App</a>
-    </Container>
+      <a href="https://github.com/ccroberts1/project3.git">Github Repo</a>
+      <br />
+      <a href="https://raining-cats-and-dogs.herokuapp.com/">
+        Heroku Deployed App
+      </a>
+    </div>
   );
 };
 export default Footer;
